52e9c7c30983af3eea4f207487b265aa282f236e
Argunov Fedor <fargunov@internet.ru>
2022-04-21 07:54:00 +0300

    Модуль 1 Добавить решение с роутами

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,4 +1,7 @@
 import datetime
+import random
+import string
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,29 +9,54 @@ app = Flask(__name__)
 
 @app.route('/hello_world')
 def test_function():
-   pass
+    return 'Привет мир!'
 
 
 @app.route('/cars')
-def test_function():
-   pass
+def cars():
+    return 'Chevrolet, Renault, Ford, Lada'
+
+
+cats_list = ['корниш рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
 
 
 @app.route('/cats')
-def test_function():
-   pass
+def cats():
+    global cats_list
+    return str(random.choice(cats_list))
 
 
 @app.route('/get_time/now')
-def test_function():
-   pass
+def get_time_now():
+    return str(datetime.datetime.now())
 
 
 @app.route('/get_time/future')
-def test_function():
-   pass
+def get_time_future():
+    return str(datetime.datetime.now() + datetime.timedelta(hours=1))
+
+
+count = 0
+
+@app.route('/counter')
+def counter():
+    global count
+    count += 1
+    return str(count)
+
+
+tab = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+
+war_and_peace = open('war_and_peace.txt', 'r', encoding='utf-8') \
+    .read() \
+    .translate(tab) \
+    .split()
 
 
 @app.route('/get_random_word')
-def test_function():
-   pass
+def get_random_word():
+    global war_and_peace
+    return random.choice(war_and_peace)
+
+
+app.run(debug=True, host='localhost', port=8080)
