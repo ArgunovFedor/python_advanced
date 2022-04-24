dc64ab9e112933220621d3a15158d09c9470ff02
Аргунов Федор Михайлович <fargunov@internet.ru>
2022-04-25 07:45:46 +0900

    Модуль 3 Добавить в тест проверку корректности вернувшегося дня недели

--- module_03_ci_culture_beginning/materials/testing_age/tests/test_social_age.py
+++ module_03_ci_culture_beginning/materials/testing_age/tests/test_social_age.py
@@ -10,8 +10,33 @@ class TestSocialAge(unittest.TestCase):
         function_res = get_social_status(age)
         self.assertEqual(expected_res, function_res)
 
+
+    def test_can_get_teenager(self):
+        age = 13
+        expected_res = 'подросток'
+        function_res = get_social_status(age)
+        self.assertEqual(expected_res, function_res)
+
+    def test_can_get_man(self):
+        age = 27
+        expected_res = 'взрослый'
+        function_res = get_social_status(age)
+        self.assertEqual(expected_res, function_res)
+
+    def test_can_get_old_man(self):
+        age = 55
+        expected_res = 'пожилой'
+        function_res = get_social_status(age)
+        self.assertEqual(expected_res, function_res)
+
+    def test_can_get_too_old_man(self):
+        age = 100
+        expected_res = 'пенсионер'
+        function_res = get_social_status(age)
+        self.assertEqual(expected_res, function_res)
+
+
     def test_cannot_pass_str_as_age(self):
         age = 'old'
         with self.assertRaises(ValueError):
             get_social_status(age)
-
