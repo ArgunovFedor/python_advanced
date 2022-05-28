f9e6e7dff3a3f810e0f6efaf69aff9bc7761b0a8
Аргунов Федор Михайлович <argunov_fm@albank.ru>
2022-05-28 12:26:34 +0300

    Модуль 5 Добавить решение

--- module_05_processes_and_threads/homework/third_block/test_context_manager.py
+++ module_05_processes_and_threads/homework/third_block/test_context_manager.py
@@ -1,13 +1,16 @@
-import io
+import sys
+from typing import IO
 
 
-class TestContextManager():
-    def __init__(self, stream: io):
+class TestContextManager:
+    def __init__(self, stream: IO):
         self.input_output = stream
+        self.__stdin = sys.stdin
+        self.__stdout = sys.stdout
 
     def __enter__(self):
         return self.input_output
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
-
+        sys.stdout = self.__stdout
+        sys.stdin = self.__stdin
