78ef215d81e5de9b7f4ef571e4d83e8e59194914
Аргунов Федор Михайлович <fargunov@internet.ru>
2022-06-17 18:12:43 +0900

    Модуль 6 Добавить 2 задание

--- module_06_debugging_begin/hw/hw_2_execute_code_from_form.py
+++ module_06_debugging_begin/hw/hw_2_execute_code_from_form.py
@@ -9,16 +9,23 @@
 4. Добавьте необходимые debug сообщения
 5. Инициализируйте basicConfig для записи логов в stdout с указанием времени
 """
-
+import logging
 import shlex
 import subprocess
 
+import flask
 from flask import Flask
+
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
 from wtforms.validators import InputRequired
 
 app = Flask(__name__)
+
+
+@app.errorhandler(OSError)
+def handle_os_exception(exc: OSError):
+    logging.exception('OSError', exc_info=exc)
 
 
 class CodeForm(FlaskForm):
@@ -53,6 +60,17 @@ def run_code():
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        filename='exec.log',
+        level=logging.DEBUG,
+        format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
+        datefmt='%H:%M:%S'
+    )
+    with open('stderr.log', 'r') as file:
+        if len(file.readlines()) == 0:
+            logging.error('That is fine')
+        else:
+            logging.error('That is not fine')
     app.config["WTF_CSRF_ENABLED"] = False
 
     app.run(debug=True)
