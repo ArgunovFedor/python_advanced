f02f11065fec3b16767f719abf5f8593bdf24f5f
Аргунов Федор Михайлович <fargunov@internet.ru>
2023-07-08 14:57:30 +0900

    Модуль 13 Добавить решение 1 задачи

--- module_13_db2/homework/hw1/main.py
+++ module_13_db2/homework/hw1/main.py
@@ -1,11 +1,22 @@
 import sqlite3
 
+request_sql = """
+SELECT COUNT(*)
+    FROM table_truck_with_vaccine
+    WHERE truck_number = ?
+        AND (temperature_in_celsius NOT BETWEEN 16 AND 20)
+"""
+
 
 def check_if_vaccine_has_spoiled(
         cursor: sqlite3.Cursor,
         truck_number: str
 ) -> bool:
-    ...
+    cursor.execute(request_sql, (truck_number,))
+    result, *_ = cursor.fetchone()
+    if result >= 3:
+        return True
+    return False
 
 
 if __name__ == '__main__':
