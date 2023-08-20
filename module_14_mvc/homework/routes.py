b002a6f4500137990e6ddb988edfe0baaecb7cff
Аргунов Федор Михайлович <fargunov@internet.ru>
2023-08-20 17:43:24 +0900

    Модуль 14 Добавить решение 2 задачи

--- module_14_mvc/homework/routes.py
+++ module_14_mvc/homework/routes.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from typing import List
-
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField
+from wtforms.validators import DataRequired, Email, NumberRange, Regexp, InputRequired
 from models import init_db, get_all_books, DATA, add_book
 
 app: Flask = Flask(__name__)
@@ -29,6 +31,11 @@ def _get_html_table_for_books(books: List[dict]) -> str:
     return table.format(books_rows=rows)
 
 
+class AddBookWithAuthorForm(FlaskForm):
+    author = StringField(name='author_name', validators=[InputRequired()])
+    book = StringField(name='book_title', validators=[InputRequired()])
+
+
 @app.route('/books')
 def all_books() -> str:
     return render_template(
@@ -40,15 +47,19 @@ def all_books() -> str:
 @app.route('/books/form', methods=['GET', 'POST'])
 def get_books_form() -> str:
     if request.method == 'POST':
-        author = request.form.get('author_name')
-        book = request.form.get('book_title')
-        if author is not None and book is not None:
-            add_book(author, book)
-        return render_template('add_book.html')
+        form = AddBookWithAuthorForm()
+        if form.validate_on_submit():
+            author = form.author.data
+            book = form.book.data
+            if author is not None and book is not None:
+                add_book(author, book)
+        else:
+            return render_template('add_book.html', form=form)
     else:
         return render_template('add_book.html')
 
 
 if __name__ == '__main__':
+    app.config["WTF_CSRF_ENABLED"] = False
     init_db(DATA)
     app.run(debug=True)
