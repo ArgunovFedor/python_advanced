8e835c40ef17dbb035b933008dccab7bda63fc43
Аргунов Федор Михайлович <fargunov@internet.ru>
2023-10-07 17:40:55 +0900

    Модуль 21 Добавить решение

---
+++ module_21_orm_2/homework/app/routes.py
@@ -0,0 +1,49 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+from module_20_orm_1.homework.app.models import Book, ReceivingBook, Student
+from module_20_orm_1.homework.app.schemas import BookSchema, StudentSchema, ReceivingBookSchema
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/api/students/debtors', methods=['GET'])
+def debtors():
+    result = []
+    debtors = ReceivingBook.get_debtors()
+    for debtor in debtors:
+        result.append(Student.get_by_id(debtor.student_id))
+    schema = StudentSchema()
+    return schema.dump(result, many=True), 200
+
+
+@app.route('/api/books/give_to_student', methods=['POST'])
+def give_to_student():
+    book_id = request.args.get('book_id')
+    id_student = request.args.get('id_student')
+    schema = ReceivingBookSchema()
+    item = ReceivingBook.give_to_student(id_book=book_id, id_student=id_student)
+    return schema.dump(item), 200
+
+
+@app.route('/api/books/hand_over_the_book', methods=['POST'])
+def hand_over_the_book():
+    book_id = request.args.get('book_id')
+    id_student = request.args.get('id_student')
+    schema = ReceivingBookSchema()
+    item = ReceivingBook.hand_over_the_book(id_book=book_id, id_student=id_student)
+    return schema.dump(item, many=True), 200
+
+
+class BookList(Resource):
+    def get(self) -> tuple[list[dict], int]:
+        schema = BookSchema()
+        books = Book.get_all_books()
+        return schema.dump(books, many=True), 200
+
+
+api.add_resource(BookList, '/api/books')
+
+if __name__ == '__main__':
+    app.run(debug=True)
