9d16c9c081d729563d523686d93f50863a7771ea
Аргунов Федор Михайлович <fargunov@internet.ru>
2023-10-22 17:54:38 +0900

    22 Add routes

--- module_22_celery/homework/app.py
+++ module_22_celery/homework/app.py
@@ -1,3 +1,35 @@
 """
 В этом файле будет ваше Flask-приложение
 """
+
+from celery import Celery
+from flask import Flask
+
+app = Flask(__name__)
+
+# Конфигурация Celery
+celery = Celery(
+    app.name,
+    broker='redis://localhost:6379/0',
+    backend='redis://localhost:6379/0',
+)
+
+
+@app.route('/blur', methods=['POST'])
+def blur():
+    pass
+
+
+@app.route('/status/<id>', methods=['GET'])
+def status(id):
+    pass
+
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    pass
+
+
+@app.route('/unsubscribe', methods=['POST'])
+def unsubscribe():
+    pass
